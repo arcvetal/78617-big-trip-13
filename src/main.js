291c15a958add54d581a7c40ffe8dc4338656ecb
Artsybashev Vitalii <arcvetal@gmail.com>
2020-11-24 21:10:15 +0400

    Исправил замечания по первому модулю

--- src/main.js
+++ src/main.js
@@ -1,20 +1,20 @@
-import {addNewTripPointTemplate} from './view/add-point.js';
-import {createEditTripPointTemplate} from './view/edit-point.js';
+import {addNewTripPointTemplate} from './view/add-trip-point.js';
+import {createEditTripPointTemplate} from './view/edit-trip-point.js';
 import {createFiltersTemplate} from './view/filter.js';
 import {createListSortTemplate} from './view/list-sort.js';
 import {createMenuTemplate} from './view/menu.js';
-import {createTripPointsListTemplate} from './view/points-list.js';
-import {createTripPointTemplate} from './view/point.js';
+import {createTripPointsListTemplate} from './view/trip-points-list.js';
+import {createTripPointTemplate} from './view/trip-point.js';
 import {createTripInfoTemplate} from './view/trip-info.js';
 import {createTripPriceTemplate} from './view/trip-price.js';
 
 const POINTS_COUNT = 3;
 
-const tripMain = document.querySelector('.trip-main');
-const tripMenuTitle = document.querySelector('.trip-controls :first-child');
-const tripFilterTitle = document.querySelector('.trip-controls :last-child');
-const tripEventsTitle = document.querySelector('.trip-events h2');
-const tripEventsSection = document.querySelector('.trip-events');
+const tripMainElement = document.querySelector('.trip-main');
+const tripMenuTitleElement = document.querySelector('.trip-controls :first-child');
+const tripFilterTitleElement = document.querySelector('.trip-controls :last-child');
+const tripEventsTitleElement = document.querySelector('.trip-events h2');
+const tripEventsSectionElement = document.querySelector('.trip-events');
 
 // Рендер компонента
 const render = (targetEl, template, position) => {
@@ -22,33 +22,33 @@ const render = (targetEl, template, position) => {
 };
 
 // Добавмим информацию о маршруте
-render(tripMain, createTripInfoTemplate(), 'afterbegin');
+render(tripMainElement, createTripInfoTemplate(), 'afterbegin');
 
 // Добавим стоимость поездки
-render(tripMain, createTripPriceTemplate(), 'beforeend');
+render(tripMainElement, createTripPriceTemplate(), 'beforeend');
 
 // Добавим меню
-render(tripMenuTitle, createMenuTemplate(), 'afterend');
+render(tripMenuTitleElement, createMenuTemplate(), 'afterend');
 
 // Добавим фильтры
-render(tripFilterTitle, createFiltersTemplate(), 'afterend');
+render(tripFilterTitleElement, createFiltersTemplate(), 'afterend');
 
 // Добавим сортировку
-render(tripEventsTitle, createListSortTemplate(), 'afterend');
+render(tripEventsTitleElement, createListSortTemplate(), 'afterend');
 
 // Добавим <ul> для будущего списка пунктов
-render(tripEventsSection, createTripPointsListTemplate(), 'beforeend');
+render(tripEventsSectionElement, createTripPointsListTemplate(), 'beforeend');
 
 // Найдем только что добавленный <ul>
-const tripPointsList = document.querySelector('.trip-events__list');
+const tripPointsListElement = document.querySelector('.trip-events__list');
 
 // Добавим форму редактора пункта
-render(tripPointsList, createEditTripPointTemplate(), 'afterbegin');
+render(tripPointsListElement, createEditTripPointTemplate(), 'afterbegin');
 
 // Добавим форму добавления нового пункта
-render(tripPointsList, addNewTripPointTemplate(), 'beforeend');
+render(tripPointsListElement, addNewTripPointTemplate(), 'beforeend');
 
 // Добавим список пунктов
 for (let i = 0; i < POINTS_COUNT; i++) {
-  render(tripPointsList, createTripPointTemplate(), 'beforeend');
+  render(tripPointsListElement, createTripPointTemplate(), 'beforeend');
 }
