999912a3f9da504192a4be827b0b9be2fb1af722
Artsybashev Vitalii <arcvetal@gmail.com>
2020-11-27 20:50:57 +0400

    Правки

--- src/mock/mocks.js
+++ src/mock/mocks.js
@@ -3,7 +3,7 @@ import dayjs from 'dayjs';
 const getRandomInt = (a = 0, b = 1) => {
   const lower = Math.ceil(Math.min(a, b));
   const upper = Math.floor(Math.max(a, b));
-  return Math.floor(lower + Math.random() * (upper - lower + 1))
+  return Math.floor(lower + Math.random() * (upper - lower + 1));
 };
 
 // Генерация рандомного типа точки маршрута
